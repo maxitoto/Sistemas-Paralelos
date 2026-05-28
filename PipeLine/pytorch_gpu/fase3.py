c0014ce544eaca0a7359fcd4b325cc1b2ddf1e82
maxitoto <tommyush2000@hotmail.com>
2026-05-28 13:06:09 -0300

    update

--- PipeLine/pytorch_gpu/fase3.py
+++ PipeLine/pytorch_gpu/fase3.py
@@ -18,16 +18,24 @@ def procesar(matrices, config):
     args, nombre_pipeline = config
     nombre_imagen = args.get("input", "imagen").split("/")[-1].split(".")[0]
 
-    cantidad_blancos = np.count_nonzero(matriz_sobel_cpu == 255)
-    total_pixeles = matriz_sobel_cpu.size
-    porcentaje_blancos = (float(cantidad_blancos) / total_pixeles) * 100.0
-    
     address = os.path.join(nombre_pipeline, nombre_imagen)
-    os.makedirs(address, exist_ok=True) 
+    porcentaje_blancos = white_percentage(matriz_sobel_cpu)
+
+    if not os.path.exists(address):
+        os.makedirs(address)
+    else:
+        print(f"    [Fase 3] La carpeta '{address}/' ya estaba creada")
+        return porcentaje_blancos, t_transfer_out # Devolvemos tiempo también aquí
     
     Image.fromarray(matriz_gris_cpu).save(os.path.join(address, "imagen_gris.png"))
     Image.fromarray(matriz_sobel_cpu).save(os.path.join(address, "imagen_sobel.png"))
     
     print(f"\n [+] Las 2 imágenes fueron exportadas en la carpeta '{address}/'")
 
+    # IMPORTANTE: Devolvemos una tupla con el tiempo
     return porcentaje_blancos, t_transfer_out
+
+def white_percentage(gray: np.ndarray) -> float:
+    total_pixels = gray.size
+    white_pixels = np.count_nonzero(gray == 255)
+    return float(white_pixels) * 100.0 / float(total_pixels)
