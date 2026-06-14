16a7e83bb22d81ce62e29d224b4b3d846b3fed18
maxitoto <tommyush2000@hotmail.com>
2026-06-14 04:56:11 -0300

    elimino carpeta auxiliar

--- TrabajoFinalRefinado/d/tools/numba_gpu/t16_16/process.py
+++
@@ -1,8 +0,0 @@
-from tools.numba_gpu.core.process import BasePipeline
-
-class Pipeline(BasePipeline):
-    def __init__(self, config):
-
-        super().__init__(config)
-        
-        self.threadsperblock = (16, 16, 1)
